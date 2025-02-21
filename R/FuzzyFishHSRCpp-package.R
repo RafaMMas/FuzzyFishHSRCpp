4f3951f45510a5d83208ec233a358b2a7b15ef7a
RafaMMas <rafa.m.mas@gmail.com>
2025-02-21 20:32:38 +0100

    Fixed NAMESPACE and others

--- R/FuzzyFishHSRCpp-package.R
+++ R/FuzzyFishHSRCpp-package.R
@@ -1,6 +1,10 @@
 #' Package-level documentation
 #'
+#' @name FuzzyFishHSRCpp
+#' @docType package
+#' 
 #' @useDynLib FuzzyFishHSRCpp, .registration = TRUE
 #' @importFrom Rcpp evalCpp
 #' @exportPattern "^[[:alpha:]]+"
 NULL
+
