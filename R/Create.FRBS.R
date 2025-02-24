64170d66ef7120feecf7f047d01fdee1a5cc1ac7
RafaMMas <rafa.m.mas@gmail.com>
2025-02-24 16:52:17 +0100

    Tried to fix problme with data.

--- R/Create.FRBS.R
+++ R/Create.FRBS.R
@@ -49,35 +49,3 @@
 Create.FRBS.R <- function(ImpVariables, Range, MFfunction, MFparameters, Consequents) {
   list(ImpVariables = ImpVariables, Range = Range, MFfunction = MFfunction, MFparameters = MFparameters, Consequents = Consequents)
 }
-
-#' Lepomis gibbosus Dataset  
-#'  
-#' This dataset contains four microhabitat variables: Velocity, Depth, Substrate, and Cover,  
-#' along with the presence (1) or absence (0) of *Lepomis gibbosus*.  
-#'  
-#' @format A data frame with X rows and Y columns:  
-#' \describe{  
-#'   \item{Velocity}{Water velocity at the sampling site (units?)}  
-#'   \item{Depth}{Water depth at the sampling site (units?)}  
-#'   \item{Substrate}{Type of substrate present (categorical or numerical?)}  
-#'   \item{Cover}{Presence of cover structures (categorical or numerical?)}  
-#'   \item{Presence}{Binary indicator: 1 (present) or 0 (absent)}  
-#' }  
-#'  
-#' @source Source of the dataset (e.g., study, publication, or fieldwork details).  
-#' @name Lepomis.gibbosus.dataset  
-#' @docType data  
-#' @keywords datasets  
-#' @usage Lepomis.gibbosus.dataset  
-NULL  
-
-#' Optimal Fuzzy Rule-Based System (FRBS) Model for *Lepomis gibbosus*  
-#'  
-#' This object represents the optimal FRBS model developed for predicting the habitat suitability  
-#' of *Lepomis gibbosus* based on key environmental variables.  
-#'  
-#' @name Lepomis.gibbosus.FRBS  
-#' @docType data  
-#' @keywords datasets  
-#' @usage Lepomis.gibbosus.FRBS  
-NULL  
---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Lepomis gibbosus Dataset  
+#'  
+#' This dataset contains four microhabitat variables: Velocity, Depth, Substrate, and Cover,  
+#' along with the presence (1) or absence (0) of *Lepomis gibbosus*.  
+#'  
+#' @format A data frame with 1293 rows and five columns. The most important used to develop the FRBS are:  
+#' \describe{  
+#'   \item{Velocity}{Water velocity at the sampling site (units?)}  
+#'   \item{Depth}{Water depth at the sampling site (units?)}  
+#'   \item{Substrate}{Type of substrate present (categorical or numerical?)}  
+#'   \item{Cover}{Presence of cover structures (categorical or numerical?)}  
+#'   \item{Presence}{Binary indicator: 1 (present) or 0 (absent)}  
+#' }  
+#'  
+#' @source Source of the dataset (e.g., study, publication, or fieldwork details).  
+#' @name Lepomis.gibbosus.dataset  
+#' @docType data  
+#' @keywords datasets  
+#' @usage data(Lepomis.gibbosus.dataset)  
+NULL  
+
+#' Optimal Fuzzy Rule-Based System (FRBS) Model for *Lepomis gibbosus*  
+#'  
+#' This object represents the optimal FRBS model developed for predicting the habitat suitability  
+#' of *Lepomis gibbosus* based on key environmental variables.  
+#'  
+#' @name Lepomis.gibbosus.FRBS  
+#' @docType data  
+#' @keywords datasets  
+#' @usage data(Lepomis.gibbosus.FRBS)  
+NULL  
