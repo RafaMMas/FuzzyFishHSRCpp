03d3bd364e5f6c4b26bf49a729937cf5badd4d64
RafaMMas <rafa.m.mas@gmail.com>
2025-02-24 17:30:02 +0100

    improved documentations

--- R/data.R
+++ R/data.R
@@ -1,31 +1,56 @@
-#' Lepomis gibbosus Dataset  
-#'  
-#' This dataset contains four microhabitat variables: Velocity, Depth, Substrate, and Cover,  
-#' along with the presence (1) or absence (0) of *Lepomis gibbosus*.  
-#'  
-#' @format A data frame with 1293 rows and five columns. The most important used to develop the FRBS are:  
-#' \describe{  
-#'   \item{Velocity}{Water velocity at the sampling site (units?)}  
-#'   \item{Depth}{Water depth at the sampling site (units?)}  
-#'   \item{Substrate}{Type of substrate present (categorical or numerical?)}  
-#'   \item{Cover}{Presence of cover structures (categorical or numerical?)}  
-#'   \item{Presence}{Binary indicator: 1 (present) or 0 (absent)}  
-#' }  
-#'  
-#' @source Source of the dataset (e.g., study, publication, or fieldwork details).  
-#' @name Lepomis.gibbosus.dataset  
-#' @docType data  
-#' @keywords datasets  
-#' @usage data(Lepomis.gibbosus.dataset)  
-NULL  
+#' Microhabitat use of Lepomis gibbosus
+#'
+#' This dataset contains environmental and habitat-related variables for species observations, including velocity, depth, and various cover types.
+#'
+#' @format A data frame with 11 variables:
+#' 
+#' \describe{
+#'   \item{Species}{Binary variable indicating species presence (1) or absence (0).}
+#'   \item{Velocity}{Water velocity (m/s).}
+#'   \item{Depth}{Water depth (m).}
+#'   \item{Substrate.index}{Index representing mean substrate type, ranging from 0 to 8.}
+#'   \item{Caves}{Binary variable indicating the presence (1) or absence (0) of caves.}
+#'   \item{Reeds}{Binary variable indicating the presence (1) or absence (0) of reeds.}
+#'   \item{Aqu.veg.}{Binary variable indicating the presence (1) or absence (0) of aquatic vegetation.}
+#'   \item{Shade}{Binary variable indicating the presence (1) or absence (0) of shade.}
+#'   \item{Rocks}{Binary variable indicating the presence (1) or absence (0) of rocks.}
+#'   \item{Wood.logs}{Binary variable indicating the presence (1) or absence (0) of wood logs.}
+#'   \item{Cover.index}{Index representing the degree of cover, ranging from 0 to 1.}
+#' }
+#'
+#' @usage data(Lepomis.gibbosus.dataset)
+#' 
+#' @references
+#' Mu\ifelse{html}{\out{&ntilde;}}{\enc{UTF-8}{ñ}}oz-Mas, R., Macian-Sorribes, H., Oliva-Paterna, F. J.,
+#' Sangelantoni, L., Peano, D., Pulido-Velazquez, M., & Mart\ifelse{html}{\out{&iacute;}}{\enc{UTF-8}{í}}nez-Capel, F. (2024).
+#' Adaptation measures to global change in the Serpis River Basin (Spain): An evaluation considering agricultural benefits,
+#' environmental flows, and invasive fishes. \emph{Ecological Indicators}, 161, 111979.
+#' DOI: 10.1016/j.ecolind.2024.111979
+#' 
+#' @examples
+#' data(Lepomis.gibbosus.dataset)
+#' summary(Lepomis.gibbosus.dataset)
+#'
+"Lepomis.gibbosus.dataset"
+
 
 #' Optimal Fuzzy Rule-Based System (FRBS) Model for *Lepomis gibbosus*  
 #'  
 #' This object represents the optimal FRBS model developed for predicting the habitat suitability  
-#' of *Lepomis gibbosus* based on key environmental variables.  
+#' of *Lepomis gibbosus* based velocity, depth, substrate and cover.  
 #'  
-#' @name Lepomis.gibbosus.FRBS  
-#' @docType data  
-#' @keywords datasets  
+#' @name Lepomis.gibbosus.FRBS
+#'   
+#' @docType list
+#'   
+#' @keywords datasets
+#' 
+#' @references
+#' Mu\ifelse{html}{\out{&ntilde;}}{\enc{UTF-8}{ñ}}oz-Mas, R., Macian-Sorribes, H., Oliva-Paterna, F. J.,
+#' Sangelantoni, L., Peano, D., Pulido-Velazquez, M., & Mart\ifelse{html}{\out{&iacute;}}{\enc{UTF-8}{í}}nez-Capel, F. (2024).
+#' Adaptation measures to global change in the Serpis River Basin (Spain): An evaluation considering agricultural benefits,
+#' environmental flows, and invasive fishes. \emph{Ecological Indicators}, 161, 111979.
+#' DOI: 10.1016/j.ecolind.2024.111979
+#'
 #' @usage data(Lepomis.gibbosus.FRBS)  
 NULL  
