ccb4d110db95fe542d15637109cd68066a5e52bd
RafaMMas <rafa.m.mas@gmail.com>
2025-06-22 18:54:16 +0200

    I fixed some errors

--- R/FuzzyFishHSRCpp-package.R
+++ R/FuzzyFishHSRCpp-package.R
@@ -1,10 +1,8 @@
 #' Package-level documentation
 #'
-#' @name FuzzyFishHSRCpp
-#' @docType package
-#' 
 #' @useDynLib FuzzyFishHSRCpp, .registration = TRUE
 #' @importFrom Rcpp evalCpp
 #' @exportPattern "^[[:alpha:]]+"
-NULL
+#' @keywords internal
+"_PACKAGE"
 
